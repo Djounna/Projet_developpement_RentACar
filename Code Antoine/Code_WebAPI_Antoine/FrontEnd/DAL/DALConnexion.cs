81b2c88cbca0396d062ebc7c5511658b5c434e5c
Hallet Antoine <hallet.antoine@outlook.com>
2022-02-25 08:31:35 +0100

    delete ancien fichier

--- Code Antoine/Code_WebAPI_Antoine/FrontEnd/DAL/DALConnexion.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace FrontEnd.DAL
-{
-    public class DALConnexion
-    {
-        private static string sConnection = @"Server=DESKTOP-FK2QPRH\SQLDB2022; Database=ProjetSGDB; User Id=sa; Password=SQLDB2022";
-
-        public static string Connexion { get { return sConnection; } }
-    }
-}
